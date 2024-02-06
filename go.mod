b76de37384ed9c6982ac1f61e3c9c1a66ca36174
sujit <s.baniya.np@gmail.com>
2024-02-06 22:53:59 +0545

    Add data migration for multiple tables from JSON/YAML Config

--- go.mod
+++ go.mod
@@ -1,48 +1,50 @@
 module github.com/oarkflow/etl
 
-go 1.20
+go 1.21.0
 
 require (
 	github.com/oarkflow/errors v0.0.6
-	github.com/oarkflow/metadata v0.0.29
-	github.com/oarkflow/pkg v0.0.69
-	gorm.io/gorm v1.25.2
+	github.com/oarkflow/metadata v0.0.34
+	github.com/oarkflow/pkg v0.1.1
+	gopkg.in/yaml.v3 v3.0.1
+	gorm.io/gorm v1.25.7
 )
 
 require (
-	github.com/bytedance/gopkg v0.0.0-20230531144706-a12972768317 // indirect
-	github.com/bytedance/sonic v1.9.2 // indirect
-	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/bytedance/gopkg v0.0.0-20240202110943-5e26950c5e57 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/goccy/go-reflect v1.2.0 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/gosimple/slug v1.13.1 // indirect
+	github.com/gosimple/unidecode v1.0.1 // indirect
 	github.com/hetiansu5/urlquery v1.2.7 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.4.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
+	github.com/jackc/pgx/v5 v5.5.3 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/klauspost/compress v1.16.7 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+	github.com/klauspost/compress v1.17.6 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
-	github.com/microsoft/go-mssqldb v1.3.0 // indirect
-	github.com/oarkflow/db v0.0.0-20230322151631-c3150713c9f8 // indirect
-	github.com/oarkflow/frame v0.0.49 // indirect
-	github.com/oarkflow/log v1.0.73 // indirect
-	github.com/oarkflow/protocol v0.0.2 // indirect
-	github.com/oarkflow/xid v1.2.5 // indirect
-	github.com/puzpuzpuz/xsync v1.5.2 // indirect
-	github.com/toorop/go-dkim v0.0.0-20201103131630-e1cd1a0a5208 // indirect
-	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
-	github.com/xhit/go-simple-mail/v2 v2.15.0 // indirect
-	golang.org/x/arch v0.4.0 // indirect
-	golang.org/x/crypto v0.11.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/text v0.11.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
-	gorm.io/driver/mysql v1.5.1 // indirect
-	gorm.io/driver/postgres v1.5.2 // indirect
-	gorm.io/driver/sqlserver v1.5.1 // indirect
+	github.com/microsoft/go-mssqldb v1.6.0 // indirect
+	github.com/oarkflow/db v0.0.1 // indirect
+	github.com/oarkflow/frame v0.0.59 // indirect
+	github.com/oarkflow/json v0.0.4 // indirect
+	github.com/oarkflow/log v1.0.74 // indirect
+	github.com/oarkflow/protocol v0.0.15 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/toorop/go-dkim v0.0.0-20240103092955-90b7d1423f92 // indirect
+	github.com/xhit/go-simple-mail/v2 v2.16.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/exp v0.0.0-20240205201215-2c58cdc269a3 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
+	gorm.io/driver/mysql v1.5.4 // indirect
+	gorm.io/driver/postgres v1.5.6 // indirect
+	gorm.io/driver/sqlserver v1.5.3 // indirect
 )
---
+++ migrate/migrate.go
@@ -0,0 +1,187 @@
+package migrate
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/oarkflow/metadata"
+	"github.com/oarkflow/pkg/evaluate"
+	"gopkg.in/yaml.v3"
+
+	"github.com/oarkflow/etl"
+	"github.com/oarkflow/etl/mapper"
+)
+
+// TableConfig contains the configuration for a single table migration.
+type TableConfig struct {
+	OldName             string            `json:"old_name" yaml:"old_name"`
+	NewName             string            `json:"new_name" yaml:"new_name"`
+	Migrate             bool              `json:"migrate" yaml:"migrate"`
+	KeepUnmatchedFields bool              `json:"keep_unmatched_fields" yaml:"keep_unmatched_fields"`
+	ExcludeFields       []string          `json:"exclude_fields" yaml:"exclude_fields"`
+	IncludeFields       []string          `json:"include_fields" yaml:"include_fields"`
+	ExtraValues         map[string]any    `json:"extra_values" yaml:"extra_values"`
+	KeyValueTable       bool              `json:"key_value_table" yaml:"key_value_table"`
+	StoreDataType       bool              `json:"store_data_type" yaml:"store_data_type"`
+	UpdateSequence      bool              `yaml:"update_sequence" json:"update_sequence"`
+	Mapping             map[string]string `json:"mapping" yaml:"mapping"`
+	Lookups             string            `json:"lookups" yaml:"lookups"`
+}
+
+func FromYaml(srcConfig, dstConfig metadata.Config, tableBytes []byte) error {
+	var tableList []TableConfig
+	err := yaml.Unmarshal(tableBytes, &tableList)
+	if err != nil {
+		return err
+	}
+	return Data(srcConfig, dstConfig, tableList)
+}
+
+func FromJson(srcConfig, dstConfig metadata.Config, tableBytes []byte) error {
+	var tableList []TableConfig
+	err := json.Unmarshal(tableBytes, &tableList)
+	if err != nil {
+		return err
+	}
+	return Data(srcConfig, dstConfig, tableList)
+}
+
+func Data(srcConfig, dstConfig metadata.Config, tableList []TableConfig) error {
+	source := metadata.New(srcConfig)
+	destination := metadata.New(dstConfig)
+
+	for _, tableConfig := range tableList {
+		if tableConfig.Migrate {
+			err := tableMigration(source, destination, tableConfig)
+			if err != nil {
+				return err
+			}
+		} else if tableConfig.OldName == "nil" {
+			fmt.Printf("Creating new entry for %s\n", tableConfig.NewName)
+			connector, err := destination.Connect()
+			if err != nil {
+				return err
+			}
+			mapping := make(map[string]any)
+			for k, v := range tableConfig.Mapping {
+				p, _ := evaluate.Parse(v, true)
+				pr := evaluate.NewEvalParams(make(map[string]interface{}))
+				val, err := p.Eval(pr)
+				if err == nil {
+					mapping[k] = val
+				}
+			}
+			err = connector.Store(tableConfig.NewName, mapping)
+			if err != nil {
+				return err
+			}
+		}
+		switch dstConfig.Driver {
+		case "postgres", "psql", "postgresql":
+			if tableConfig.UpdateSequence {
+				// update the sequence counter for the table
+				// get the primary key for a table
+				connector, err := destination.Connect()
+				if err != nil {
+					return err
+				}
+				// get the name of the sequence counter for the table
+				sql := fmt.Sprintf(
+					` SELECT column_name, table_name,
+            substring(column_default from '''([^'']+)''') AS sequence_name
+	      	FROM information_schema.columns
+      		WHERE column_default LIKE 'nextval%%' AND table_name = '%s';`, tableConfig.NewName)
+				seqResult, err := connector.GetRawCollection(sql)
+				if err != nil {
+					return err
+				}
+				// get the column_name, table_name, and sequence_name from seqResult
+				var columnName, tableName, sequenceName string
+				for _, row := range seqResult {
+					columnName = row["column_name"].(string)
+					tableName = row["table_name"].(string)
+					sequenceName = row["sequence_name"].(string)
+				}
+				// update the sequence counter for the table
+				sql2 := fmt.Sprintf("SELECT setval('%s', COALESCE((SELECT MAX(%s)+1 FROM %s), 1), false);", sequenceName, columnName, tableName)
+				err = connector.Exec(sql2)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func getLookups(source, destination metadata.DataSource, lookup string) (map[string]any, error) {
+	if lookup == "" {
+		return nil, nil
+	}
+	parts := strings.Split(lookup, ".")
+	var toLookup string
+	var useSource bool
+	if len(parts) != 2 {
+		useSource = true
+		toLookup = parts[0]
+	} else {
+		toLookup = parts[1]
+		switch parts[0] {
+		case "source":
+			useSource = true
+		case "destination":
+			useSource = false
+		default:
+			return nil, fmt.Errorf("invalid lookup %s as %s", toLookup, parts[0])
+		}
+	}
+	var connector metadata.DataSource
+	var err error
+	if useSource {
+		connector, err = source.Connect()
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		connector, err = destination.Connect()
+		if err != nil {
+			return nil, err
+		}
+	}
+	sql := fmt.Sprintf("SELECT * FROM %s", lookup)
+	lookups, err := connector.GetRawCollection(sql)
+	if err != nil {
+		return nil, err
+	}
+	return map[string]any{toLookup: lookups}, nil
+}
+
+// tableMigration migrates a single table from the source to the destination using the provided
+// table configuration.
+func tableMigration(source, destination metadata.DataSource, tableConfig TableConfig) error {
+	lookups, err := getLookups(source, destination, tableConfig.Lookups)
+	if err != nil {
+		return err
+	}
+	instance := etl.New(etl.Config{CloneSource: false, BatchSize: 500})
+	mp := mapper.New(&mapper.Config{
+		FieldMaps:           tableConfig.Mapping,
+		KeepUnmatchedFields: tableConfig.KeepUnmatchedFields,
+		Lookups:             lookups,
+	})
+
+	instance.AddSource(source, etl.Source{Name: tableConfig.OldName})
+	instance.AddTransformer(mp)
+	instance.AddDestination(destination, etl.Destination{
+		Name:          tableConfig.NewName,
+		ExcludeFields: tableConfig.ExcludeFields,
+		IncludeFields: tableConfig.IncludeFields,
+		ExtraValues:   tableConfig.ExtraValues,
+		KeyValueTable: tableConfig.KeyValueTable,
+		StoreDataType: tableConfig.StoreDataType,
+	})
+
+	_, err = instance.Process()
+	return err
+}
